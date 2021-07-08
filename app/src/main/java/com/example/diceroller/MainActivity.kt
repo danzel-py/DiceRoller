63f0a5cc6d4bab143146e1c84c8adb7f49b13415
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-07-08 16:10:56 +0900

    added image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Rolls dice when app starts
+        rollDice()
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener{
@@ -22,11 +25,26 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create new 6 sided Dice and roll it
         val dice = Dice(6)
-        val res = dice.roll()
+        val diceRes = dice.roll()
 
-        // Display the result
-        val resultTextView : TextView = findViewById(R.id.textView)
-        resultTextView.text = res.toString()
+        // Find image resource
+        val resImage : ImageView = findViewById(R.id.imageView)
+
+        // Determine which dice image
+        val diceImage = when(diceRes) {
+            1 -> (R.drawable.dice_1)
+            2 -> (R.drawable.dice_2)
+            3 -> (R.drawable.dice_3)
+            4 -> (R.drawable.dice_4)
+            5 -> (R.drawable.dice_5)
+            else -> (R.drawable.dice_6)
+        }
+
+        // Update dice image
+        resImage.setImageResource(diceImage)
+
+        // Update content desc
+        resImage.contentDescription = diceRes.toString()
     }
 
 }
